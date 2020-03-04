6dbf21f48d1cf1d9f016503112f17d142e192ecf
FilipRegenczuk <f.regenczuk@outlook.com>
2020-03-04 17:16:02 +0100

    import from desktop

---
+++ signal_reader.py
@@ -0,0 +1,56 @@
+import pylab
+import pandas
+
+
+# Function preparing .txt file with spaces to EKG .csv file
+def convert_txt_to_csv(file_txt):
+
+    header_list_ekg = "I,II,III,aVR,aVL,AVF,V3R,V1,V2,V4,V5,V6"
+
+    with open(file_txt) as file:
+        content = file.read()
+
+    content = content.replace(" ", ",")
+
+    with open(file_txt + ".csv", "w") as file:
+        file.write(header_list_ekg + "\n")
+        file.write(content)
+
+
+
+# Function printing EKG signal
+def print_signal(file_csv):
+
+    signal = pandas.read_csv(file_csv)
+    i = 0
+    pX = 0
+    pY = 0
+
+    while i <= 11:
+        
+        line = []
+        for index, row in signal.iterrows():
+            line.append(row[i])
+
+        y = line
+        x = range(0, len(y))
+
+        pylab.subplot2grid((6,2), (pX,pY))
+        pylab.plot(x,y, 'r')
+        pylab.grid(True)
+        pylab.title(signal.columns[i])
+
+        i += 1
+
+        if pX < 5:
+            pX += 1
+        else:
+            pX = 0
+            pY += 1
+        
+
+    pylab.show()
+
+
+#convert_txt_to_csv("ekg1.txt")
+print_signal("ekg1.csv")
