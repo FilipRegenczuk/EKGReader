5ca2982d29f62bf84e53c3fe9a1ea7f5253948ff
FilipRegenczuk <f.regenczuk@outlook.com>
2020-04-02 10:41:10 +0200

    ending gui

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk 
+from PIL import Image, ImageTk
 
 class Window(object):
 
@@ -8,11 +9,18 @@ class Window(object):
         # Window features
         window.title("EKG Reader")
         window.resizable(0, 0)
-        window.geometry('300x200')
+        window.geometry('310x240')
+        window.iconbitmap('images/icon.ico')
+
+        
 
         # Front:
-        labelTitle = tk.Label(window, text="EKG Reader")
+        load = Image.open('images/title.jpg')
+        render = ImageTk.PhotoImage(load)
+        labelTitle = tk.Label(window, image=render)
+        labelTitle.image = render
         labelTitle.grid(row=0, column=0, columnspan=2)
+
 
         # Buttons:
         px = 20
@@ -20,17 +28,17 @@ class Window(object):
         buttonFile = tk.Button(window, text="Enter EKG file", width=15)
         buttonFile.grid(row=1, column=0, padx=px, pady=py)
         buttonPrintAll = tk.Button(window, text="Print all signals", width=15)
-        buttonPrintAll.grid(row=2, column=0, pady=py)
+        buttonPrintAll.grid(row=2, column=0, padx=px, pady=py)
         buttonPrintOne = tk.Button(window, text="Print a signal", width=15)
-        buttonPrintOne.grid(row=2, column=1, pady=py)
+        buttonPrintOne.grid(row=2, column=1, padx=px, pady=py)
         buttonInstruction = tk.Button(window, text="Instruction", width=15)
-        buttonInstruction.grid(row=3, column=0, pady=py)
+        buttonInstruction.grid(row=3, column=0, padx=px, pady=py)
         buttonQuit = tk.Button(window, text="Quit", width=15)
-        buttonQuit.grid(row=3, column=1, pady=py)
+        buttonQuit.grid(row=3, column=1, padx=px, pady=py)
 
         # Entry:
-        entryFile = tk.Entry(window, width=15)
-        entryFile.grid(row=1, column=1)
+        entryFile = tk.Entry(window, width=18)
+        entryFile.grid(row=1, column=1, padx=px, pady=py)
 
 
 window = tk.Tk()
