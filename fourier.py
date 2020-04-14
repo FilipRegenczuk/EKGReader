654e73f3a653153439cd6b72a28da24331f12b1f
FilipRegenczuk <f.regenczuk@outlook.com>
2020-04-14 15:02:18 +0200

    some edit

--- fourier.py
+++ fourier.py
@@ -7,9 +7,13 @@ fs = 1000000
 x = np.arange(65536)
 
 phase = (x*2*np.pi*f/fs)%(2*np.pi)
-sinus = np.sin(faza)
+sinus = np.sin(phase)
 
 y = sinus
+yf = np.fft.fft(y)
 
+pylab.subplot(211)
 pylab.plot(x,y)
+pylab.subplot(212)
+pylab.plot(x,yf)
 pylab.show()
