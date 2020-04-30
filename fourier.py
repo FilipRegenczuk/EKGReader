b96bab670f4b319ed9502aa5a0d20cc21eb9b40b
FilipRegenczuk <f.regenczuk@outlook.com>
2020-04-30 12:19:03 +0200

    fourier transform edit

--- fourier.py
+++ fourier.py
@@ -3,17 +3,23 @@ import numpy as np
 
 
 f = 50
-fs = 1000000
+fs = 100000
 x = np.arange(65536)
 
 phase = (x*2*np.pi*f/fs)%(2*np.pi)
 sinus = np.sin(phase)
 
-y = sinus
-yf = np.fft.fft(y)
 
-pylab.subplot(211)
+y = sinus
+yf = np.fft.fft(y)      # fast fourier transform
+freq = np.fft.fftfreq(y.shape[-1])
+
+yf2 = np.fft.ifft(y)
+
+pylab.subplot(311)
 pylab.plot(x,y)
-pylab.subplot(212)
-pylab.plot(x,yf)
+pylab.subplot(312)
+pylab.plot(freq,yf.real, freq, yf.imag)
+pylab.subplot(313)
+pylab.plot(freq,yf2.real, freq, yf2.imag)
 pylab.show()
